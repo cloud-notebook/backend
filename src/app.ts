b0720664e7ef47117296cb19223af5260614b8ec
Ehtisham Ali <ehtisham.rajpoot.official@gmail.com>
2023-02-27 12:09:31 +0500

    Authentication and logger almost complete

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,31 @@ import express, { Application } from "express";
 import dotenv from 'dotenv';
 import errorMiddleware from "./middleware/error.middleware";
 import userRouter from "./routes/user.routes";
+import Connect from "./config/database.config";
+import logger from "./config/logger";
+import morganMiddleware from "./middleware/morgan.middleware";
+
 
 const app: Application = express();
 
 //Enviorment variable
 dotenv.config();
 
+//JSON Parser
 app.use(express.json());
 
-app.use("/api/v1/user", userRouter);
+//Database Configration
+Connect();
 
+//morgan middleware
+app.use(morganMiddleware);
+
+//Endpoints
+app.use("/api/v1/user", userRouter);
 
 //Error Middleware
 app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server Running On The Port ${process.env.PORT}`);
+    logger.info(`Server Running on port ${process.env.PORT}`);
 });
