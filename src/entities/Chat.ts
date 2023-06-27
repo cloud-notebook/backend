6154cc5fc1c11fcc17175c841fa2fa18e10e1007
Ehtisham Ali <ehtisham.rajpoot.official@gmail.com>
2023-06-27 21:29:43 +0500

    changes fro the message module

---
+++ src/entities/Chat.ts
@@ -0,0 +1,27 @@
+import { Schema, Types, model } from "mongoose";
+
+
+const chatSchema = new Schema({
+    chatName: {
+        type: String,
+        trim: true
+    },
+    users: [
+        {
+            type: Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    lastMessage: {
+        type: String,
+    },
+    messages: [
+        {
+            type: Types.ObjectId,
+            ref: "Message"
+        }
+    ]
+});
+
+
+export default model("Chat", chatSchema);
